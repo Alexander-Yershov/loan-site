4733180488d0ef283bce43a55af0b73088f90bd7
Alexander-Yershov <alex04398956@gmail.com>
2023-06-11 19:20:57 +0300

    slider-2
    реализует функционал слайдера на второй странице сайта

--- src/js/modules/slider/slider-mini.js
+++ src/js/modules/slider/slider-mini.js
@@ -3,7 +3,7 @@ import Slider from "./slider";
 export default class MiniSlider extends Slider {
     constructor(container, next, prev, activeClass, animate, autoplay) {
         super(container, next, prev, activeClass, animate, autoplay);
-    };
+    }
 
     decorizeSlides() {
         this.slides.forEach(slide => {
@@ -22,7 +22,7 @@ export default class MiniSlider extends Slider {
             this.slides[0].querySelector('.card__title').style.opacity = '1';
             this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';
         };
-    };
+    }
 
     nextSlide() {
         if (this.slides[1].tagName == 'BUTTON' && this.slides[2].tagName == 'BUTTON') {
@@ -38,7 +38,7 @@ export default class MiniSlider extends Slider {
                 this.container.appendChild(this.slides[0]);
                 this.decorizeSlides();
             }; 
-    };
+    }
 
     bindTriggers() {
         this.next.addEventListener('click', () => this.nextSlide());
@@ -55,21 +55,24 @@ export default class MiniSlider extends Slider {
             };
             
         });
-    };
+    }
     
     init() {
-        this.container.style.cssText = `
+        try {
+           this.container.style.cssText = `
             display: flex;
             flex-wrap: wrap;
             overflow: hidden;
             align-items: flex-start;
-        `;
+            `;
 
-        this.bindTriggers();
-        this.decorizeSlides();
+            this.bindTriggers();
+            this.decorizeSlides();
 
-        if (this.autoplay) {
-            setInterval(() => this.nextSlide(), 5000)
-        }
-    };
+            if (this.autoplay) {
+                setInterval(() => this.nextSlide(), 5000)
+            }; 
+        } catch (e) { };        
+        
+    }
 };
